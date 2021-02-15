3377a119c87d0576940ea5c7e73e8039e16b7c4a
Isac Canedo <32867124+isaccanedo@users.noreply.github.com>
2021-02-14 21:30:49 -0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class Main {
+	
+	/* +---------------------------------------------
+	 * | @author Isac Canedo
+	 * +---------------------------------------------
+	 */
+	
+	/*
+	 * You have an ArrayList of names that you want to sort alphabetically.
+	 * Which approach would not work?
+	 * Você tem uma ArrayList de nomes que deseja classificar em ordem alfabética.
+	 * Qual abordagem não funcionaria?
+	 * 
+	 * 
+	 * 
+	 */	
+
+	public static void main(String[] args) {
+		
+		List<String> names = new ArrayList<>();
+		names.add("F");
+		names.add("K");
+		names.add("C");
+		
+	
+		System.out.println(names.sort(Comparator.comparing););		
+		
+	}
+
+}
